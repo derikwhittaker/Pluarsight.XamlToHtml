c47e4b8d3523afa534d36fdaa34f74c68f9e0808
Derik Whittaker <derik@graudo.com>
2013-02-07 20:03:33 -0500

    WIP for web project setup

---
+++ ToDo.MVC/Scripts/d.ts/references.ts
@@ -0,0 +1,4 @@
+/// <reference path="bootstrap.d.ts" />
+/// <reference path="underscore.d.ts" />
+/// <reference path="knockout.d.ts" />
+/// <reference path="jquery.d.ts" />
---
+++ ToDo.MVC/Scripts/ts/HelloworldViewModel.ts
@@ -0,0 +1,8 @@
+/// <reference path="../d.ts/references.ts" />
+
+
+module ToDo {
+    export class HelloworldViewModel {  
+        public Message: KnockoutObservableString = ko.observable("hello world");
+    }
+}
